1ca6cf39289a6dd712610bb556a0a1ebd238bda3
Renato Alves <alves.rjc@gmail.com>
2022-08-22 16:30:01 +0200

    Replace hardcoded property endpoints with dynamic ones

--- src/mattermostautodriver/driver.py
+++ src/mattermostautodriver/driver.py
@@ -1,33 +1,10 @@
 import asyncio
+import importlib
 import logging
-import warnings
+import os
 
 from .client import AsyncClient, Client
 from .websocket import Websocket
-from .endpoints.brand import Brand
-from .endpoints.channels import Channels
-from .endpoints.cluster import Cluster
-from .endpoints.commands import Commands
-from .endpoints.compliance import Compliance
-from .endpoints.files import Files
-from .endpoints.ldap import LDAP
-from .endpoints.oauth import OAuth
-from .endpoints.posts import Posts
-from .endpoints.preferences import Preferences
-from .endpoints.reactions import Reactions
-from .endpoints.saml import SAML
-from .endpoints.system import System
-from .endpoints.teams import Teams
-from .endpoints.users import Users
-from .endpoints.webhooks import Webhooks
-from .endpoints.elasticsearch import Elasticsearch
-from .endpoints.emoji import Emoji
-from .endpoints.data_retention import DataRetention
-from .endpoints.roles import Roles
-from .endpoints.status import Status
-from .endpoints.bots import Bots
-from .endpoints.opengraph import Opengraph
-from .endpoints.integration_actions import IntegrationActions
 
 log = logging.getLogger("mattermostautodriver.api")
 log.setLevel(logging.INFO)
@@ -60,29 +37,60 @@ class BaseDriver:
         "proxy": None,
     }
     """
-	Required options
-		- url
+    Required options
+        - url
 
-	Either
-		- login_id
-		- password
+    Either
+        - login_id
+        - password
 
-	Or
-		- token (https://docs.mattermost.com/developer/personal-access-tokens.html)
+    Or
+        - token (https://docs.mattermost.com/developer/personal-access-tokens.html)
 
-	Optional
-		- scheme ('https')
-		- port (8065)
-		- verify (True)
-		- timeout (30)
-		- request_timeout (None)
-		- mfa_token (None)
-		- auth (None)
-		- debug (False)
+    Optional
+        - scheme ('https')
+        - port (8065)
+        - verify (True)
+        - timeout (30)
+        - request_timeout (None)
+        - mfa_token (None)
+        - auth (None)
+        - debug (False)
 
-	Should not be changed
-		- basepath ('/api/v4') - unlikely this would do any good
-	"""
+    Should not be changed
+        - basepath ('/api/v4') - unlikely this would do any good
+    """
+
+    def __new__(cls, *args, **kwargs):
+        cls._initialize_endpoints(cls)
+        return super().__new__(cls, *args, **kwargs)
+
+    def _initialize_endpoints(cls):
+        module_path = os.path.dirname(os.path.abspath(__file__))
+        endpoint_path = os.path.join((module_path), "endpoints")
+
+        log.debug("Module path: %s - Endpoint path: %s", module_path, endpoint_path)
+
+        for endpoint in os.listdir(endpoint_path):
+            end = os.path.splitext(os.path.basename(endpoint))[0]
+
+            if end == "base" or end.startswith("_"):
+                # Skip base endpoint and any file starting with _ (__init__)
+                continue
+
+            # Load module and find the main module class
+            # e.g. mattermostautodriver.endpoints.users -> Users
+            module = importlib.import_module(f".endpoints.{end}", __package__)
+            classnames = [x for x in dir(module) if x != "Base" and not x.startswith("_")]
+
+            assert len(classnames) == 1, f"Unexpected endpoint configuration: {end}. Please report bug"
+
+            _class = getattr(module, classnames.pop())
+
+            # Setting self.module = property(ModuleClass(self.client))
+            # Note: We need to bind the _class in the lambda scope or
+            # the function won't act as a closure
+            setattr(cls, end, property(lambda s, c=_class: c(s.client)))
 
     def __init__(self, options, client_cls):
         """
@@ -104,222 +112,6 @@ class BaseDriver:
     def disconnect(self):
         """Disconnects the driver from the server, stopping the websocket event loop."""
         self.websocket.disconnect()
-
-    @property
-    def users(self):
-        """
-        Api endpoint for users
-
-        :return: Instance of :class:`~endpoints.users.Users`
-        """
-        return Users(self.client)
-
-    @property
-    def teams(self):
-        """
-        Api endpoint for teams
-
-        :return: Instance of :class:`~endpoints.teams.Teams`
-        """
-        return Teams(self.client)
-
-    @property
-    def channels(self):
-        """
-        Api endpoint for channels
-
-        :return: Instance of :class:`~endpoints.channels.Channels`
-        """
-        return Channels(self.client)
-
-    @property
-    def posts(self):
-        """
-        Api endpoint for posts
-
-        :return: Instance of :class:`~endpoints.posts.Posts`
-        """
-        return Posts(self.client)
-
-    @property
-    def files(self):
-        """
-        Api endpoint for files
-
-        :return: Instance of :class:`~endpoints.files.Files`
-        """
-        return Files(self.client)
-
-    @property
-    def preferences(self):
-        """
-        Api endpoint for preferences
-
-        :return: Instance of :class:`~endpoints.preferences.Preferences`
-        """
-        return Preferences(self.client)
-
-    @property
-    def emoji(self):
-        """
-        Api endpoint for emoji
-
-        :return: Instance of :class:`~endpoints.emoji.Emoji`
-        """
-        return Emoji(self.client)
-
-    @property
-    def reactions(self):
-        """
-        Api endpoint for posts' reactions
-
-        :return: Instance of :class:`~endpoints.reactions.Reactions`
-        """
-        return Reactions(self.client)
-
-    @property
-    def system(self):
-        """
-        Api endpoint for system
-
-        :return: Instance of :class:`~endpoints.system.System`
-        """
-        return System(self.client)
-
-    @property
-    def webhooks(self):
-        """
-        Api endpoint for webhooks
-
-        :return: Instance of :class:`~endpoints.webhooks.Webhooks`
-        """
-        return Webhooks(self.client)
-
-    @property
-    def compliance(self):
-        """
-        Api endpoint for compliance
-
-        :return: Instance of :class:`~endpoints.compliance.Compliance`
-        """
-        return Compliance(self.client)
-
-    @property
-    def cluster(self):
-        """
-        Api endpoint for cluster
-
-        :return: Instance of :class:`~endpoints.cluster.Cluster`
-        """
-        return Cluster(self.client)
-
-    @property
-    def brand(self):
-        """
-        Api endpoint for brand
-
-        :return: Instance of :class:`~endpoints.brand.Brand`
-        """
-        return Brand(self.client)
-
-    @property
-    def oauth(self):
-        """
-        Api endpoint for oauth
-
-        :return: Instance of :class:`~endpoints.oauth.OAuth`
-        """
-        return OAuth(self.client)
-
-    @property
-    def saml(self):
-        """
-        Api endpoint for saml
-
-        :return: Instance of :class:`~endpoints.saml.SAML`
-        """
-        return SAML(self.client)
-
-    @property
-    def ldap(self):
-        """
-        Api endpoint for ldap
-
-        :return: Instance of :class:`~endpoints.ldap.LDAP`
-        """
-        return LDAP(self.client)
-
-    @property
-    def elasticsearch(self):
-        """
-        Api endpoint for elasticsearch
-
-        :return: Instance of :class:`~endpoints.elasticsearch.Elasticsearch`
-        """
-        return Elasticsearch(self.client)
-
-    @property
-    def data_retention(self):
-        """
-        Api endpoint for data_retention
-
-        :return: Instance of :class:`~endpoints.data_retention.DataRetention`
-        """
-        return DataRetention(self.client)
-
-    @property
-    def status(self):
-        """
-        Api endpoint for status
-
-        :return: Instance of :class:`~endpoints.status.Status`
-        """
-        return Status(self.client)
-
-    @property
-    def commands(self):
-        """
-        Api endpoint for commands
-
-        :return: Instance of :class:`~endpoints.commands.Commands`
-        """
-        return Commands(self.client)
-
-    @property
-    def roles(self):
-        """
-        Api endpoint for roles
-
-        :return: Instance of :class:`~endpoints.roles.Roles`
-        """
-        return Roles(self.client)
-
-    @property
-    def opengraph(self):
-        """
-        Api endpoint for opengraph
-
-        :return: Instance of :class:`~endpoints.opengraph.Opengraph`
-        """
-        return Opengraph(self.client)
-
-    @property
-    def integration_actions(self):
-        """
-        Api endpoint for integration actions
-
-        :return: Instance of :class:`~endpoints.integration_actions.IntegrationActions`
-        """
-        return IntegrationActions(self.client)
-
-    @property
-    def bots(self):
-        """
-        Api endpoint for bots
-
-        :return: Instance of :class:`~endpoints.bots.Bots`
-        """
-        return Bots(self.client)
 
 
 class Driver(BaseDriver):
