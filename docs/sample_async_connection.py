cef132f196cd495e5b9114b37513c033689aa4a9
Renato Alves <renato.alves@embl.de>
2025-05-16 07:22:18 +0200

    Add sample files for sync and async connections

---
+++ docs/sample_async_connection.py
@@ -0,0 +1,48 @@
+from mattermostautodriver import AsyncDriver
+import asyncio
+
+
+async def my_event_handler(message):
+    print(repr(message))
+
+
+class Handler:
+    def __init__(self):
+        self.driver = AsyncDriver({
+            "url": "127.0.0.1",
+            "token": "e691u15hajdebcnqpfdceqihcc",
+            "scheme": 'http',
+            "port": 8065,
+            "verify": False,
+            # "debug": True,
+        })
+
+    async def amain(self):
+        print(">>> Login:", await self.driver.login())
+
+        task = asyncio.create_task(self.driver.init_websocket(my_event_handler))
+
+        print(">>> Get Admin:", await self.driver.users.get_user_by_username('admin'))
+
+        print(">>> Get my own details:", await self.driver.users.get_user(user_id='me'))
+
+        print(">>> Get all teams:", await self.driver.teams.get_all_teams())
+
+        print("Websocket connected, closing main thread in 5 seconds")
+        await asyncio.sleep(5)
+        print("Disconnecting")
+
+        self.driver.disconnect()
+
+        # Will disconnect as soon as a message is sent by the Mattermost server
+        # which can be a rather long time on a mostly inactive server
+        # await task
+        # alternatively use:
+        task.cancel()
+
+
+handler = Handler()
+
+asyncio.run(handler.amain())
+
+print("Disconnected")
---
+++ docs/sample_sync_connection.py
@@ -0,0 +1,70 @@
+from mattermostautodriver import Driver
+from multiprocessing import Process
+import time
+
+
+async def my_event_handler(message):
+    print(repr(message))
+
+
+class Handler:
+    def __init__(self):
+        self.driver = Driver({
+            "url": "127.0.0.1",
+            "token": "e691u15hajdebcnqpfdceqihcc",
+            "scheme": 'http',
+            "port": 8065,
+            "verify": False,
+            # "debug": True,
+        })
+
+    def login(self):
+        print(">>> Login:", self.driver.login())
+
+    def main(self):
+        self.driver.init_websocket(my_event_handler)
+
+    def requests(self):
+        print(">>> Get Admin:", self.driver.users.get_user_by_username('admin'))
+
+        print(">>> Get my own details:", self.driver.users.get_user(user_id='me'))
+
+        print(">>> Get all teams:", self.driver.teams.get_all_teams())
+
+
+def main(handler):
+    handler.main()
+
+
+
+handler = Handler()
+
+handler.login()
+
+p = Process(target=main, args=(handler,))
+p.start()
+
+handler.requests()
+
+print("Websocket connected, closing main thread in 5 seconds")
+time.sleep(5)
+print("Disconnecting")
+
+# Use `disconnect()` to disconnect the websocket
+handler.driver.disconnect()
+
+# Shutdown the handler process as gracefully as possible
+# After disconnect the socket will eventually close bit needs to receive
+# an event from the Mattermost server as the driver blocks listening for events
+
+for signal in (p.terminate, p.kill):
+    p.join(timeout=5)
+
+    if p.is_alive():
+        signal()
+    else:
+        break
+
+p.close()
+
+print("Disconnected")
